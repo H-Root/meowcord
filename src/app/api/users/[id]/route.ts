ef33e66657efbc3c4d4c904c52ebddc38e74ae9c
Nour Mahmoud <nour.m.mahmod@gmail.com>
2025-10-13 18:53:38 +0300

    Data deletion and modification operation

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -1,5 +1,5 @@
 import { TUserDB } from "@/app/type";
-import { readFileSync } from "fs";
+import { readFileSync, writeFileSync } from "fs";
 import { NextRequest } from "next/server";
 import path from "path";
 import { cwd } from "process";
@@ -7,22 +7,67 @@ import { cwd } from "process";
 const dbPath = path.join(cwd(), "src", "db.json");
 
 export async function GET(
-	_request: NextRequest,
-	{ params }: { params: Promise<{ id: string }> }
+  _request: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
 ) {
-	const readFile = readFileSync(dbPath);
+  const readFile = readFileSync(dbPath);
 
-	const db = JSON.parse(readFile.toString()) as TUserDB;
+  const db = JSON.parse(readFile.toString()) as TUserDB;
 
-	const { id } = await params;
+  const { id } = await params;
 
-	const user = db.find((user) => user.id === id);
+  const user = db.find((user) => user.id === id);
 
-	if (!user) {
-		return new Response(JSON.stringify({ message: "user not found" }), {
-			status: 404,
-		});
-	}
+  if (!user) {
+    return new Response(JSON.stringify({ message: "user not found" }), {
+      status: 404,
+    });
+  }
 
-	return Response.json(user);
+  return Response.json(user);
+}
+export async function DELETE(
+  _request: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const readFile = readFileSync(dbPath);
+  const db = JSON.parse(readFile.toString()) as TUserDB;
+  const { id } = await params;
+  const user = db.find((u) => u.id === id);
+  const users = db.filter((user) => user.id !== id);
+
+  try {
+    writeFileSync(dbPath, JSON.stringify(users), "utf-8");
+    return Response.json({ message: `Delete user : ${user!.first_name}` });
+  } catch (error) {
+    console.log(error);
+    return new Response(JSON.stringify({ message: "something went wrong" }), {
+      status: 500,
+    });
+  }
+}
+export async function PATCH(
+  request: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const readFile = readFileSync(dbPath);
+  const db = JSON.parse(readFile.toString()) as TUserDB;
+  const { id } = await params;
+  const user = db.find((u) => u.id === id);
+  const body = await request.json();
+  const { first_name } = body;
+  if (user) {
+    user.first_name = first_name;
+  } else {
+    return Response.json({ message: "User not found" }, { status: 404 });
+  }
+  try {
+    writeFileSync(dbPath, JSON.stringify(db), "utf-8");
+    return Response.json({ message: "User updated", user: { ...body } });
+  } catch (error) {
+    console.log(error);
+    return new Response(JSON.stringify({ message: "something went wrong" }), {
+      status: 500,
+    });
+  }
 }
