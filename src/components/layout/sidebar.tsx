e969c0e6217c7229d65b1bcd7a88e986b12d8aff
Hans Root <h.khateb@lucidly.ae>
2025-11-02 19:23:10 +0300

    fixes

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -1,50 +1,51 @@
 import {
-  Sidebar,
-  SidebarContent,
-  SidebarGroup,
-  SidebarGroupContent,
-  SidebarHeader,
-  SidebarMenu,
-  SidebarMenuItem,
-  SidebarSeparator,
+	Sidebar,
+	SidebarContent,
+	SidebarGroup,
+	SidebarGroupContent,
+	SidebarHeader,
+	SidebarMenu,
+	SidebarMenuItem,
+	SidebarSeparator,
 } from "@/components/ui/sidebar";
 
 import UserSidebarItem from "./user-sidebar-item";
 import CommandDialogDemo from "./CommandDialogDemo";
 import SideBarFooterSection from "./sideBarFooterSection";
 import data from "@/db.json";
-const items = data
-  .map((d) => {
-    const initials = d.first_name.charAt(0) + d.last_name.charAt(0);
-    const url = `/${d.id}`;
-    const item = { ...d, url: url, initials: initials };
-    return item;
-  })
-  .filter((t) => t.status !== "pending");
 
 export function AppSidebar() {
-  return (
-    <Sidebar>
-      <SidebarHeader>
-        <SidebarMenu>
-          <SidebarMenuItem>
-            <CommandDialogDemo items={items} />
-          </SidebarMenuItem>
-        </SidebarMenu>
-      </SidebarHeader>
-      <SidebarSeparator className="!ml-auto !w-[93%]" />
-      <SidebarContent>
-        <SidebarGroup>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              {items.map((item) => (
-                <UserSidebarItem key={item.first_name} {...item} />
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
-      </SidebarContent>
-      <SideBarFooterSection />
-    </Sidebar>
-  );
+	const items = data
+		.map((d) => {
+			const initials = d.first_name.charAt(0) + d.last_name.charAt(0);
+			const url = `/${d.id}`;
+			const item = { ...d, url: url, initials: initials };
+			return item;
+		})
+		.filter((t) => t.status !== "pending");
+
+	return (
+		<Sidebar>
+			<SidebarHeader>
+				<SidebarMenu>
+					<SidebarMenuItem>
+						<CommandDialogDemo items={items} />
+					</SidebarMenuItem>
+				</SidebarMenu>
+			</SidebarHeader>
+			<SidebarSeparator className="!ml-auto !w-[93%]" />
+			<SidebarContent>
+				<SidebarGroup>
+					<SidebarGroupContent>
+						<SidebarMenu>
+							{items.map((item) => (
+								<UserSidebarItem key={item.id} {...item} />
+							))}
+						</SidebarMenu>
+					</SidebarGroupContent>
+				</SidebarGroup>
+			</SidebarContent>
+			<SideBarFooterSection />
+		</Sidebar>
+	);
 }
--- src/components/layout/user-sidebar-item.tsx
+++ src/components/layout/user-sidebar-item.tsx
@@ -1,72 +1,72 @@
-"use client";
 import { SidebarMenuButton, SidebarMenuItem } from "../ui/sidebar";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { X } from "lucide-react";
-import { useState } from "react";
+import Link from "next/link";
 
 type Props = {
-  first_name: string;
-  last_name: string;
-  image: string;
-  url: string;
-  initials: string;
-  status: string;
+	first_name: string;
+	last_name: string;
+	image: string;
+	url: string;
+	initials: string;
+	status: string;
+	id: string | number;
 };
 
 const UserSidebarItem = ({
-  first_name,
-  last_name,
-  image,
-  initials,
-  url,
-  status,
+	first_name,
+	last_name,
+	image,
+	initials,
+	url,
+	status,
+	id,
 }: Props) => {
-  const [hovered, setHovered] = useState(false);
-  return (
-    <SidebarMenuItem key={first_name} className="user-menu-item cursor-pointer">
-      <SidebarMenuButton
-        onMouseEnter={() => setHovered(true)}
-        onMouseLeave={() => setHovered(false)}
-        className="gap-4"
-        size={"lg"}
-      >
-        <a
-          href={url}
-          className="transition-all duration-200 w-full h-full flex items-center gap-4"
-        >
-          <div className="relative w-fit">
-            <Avatar className="rounded-full border">
-              <AvatarImage alt="cn" src={image} />
-              <AvatarFallback className="rounded-full">
-                {initials}
-              </AvatarFallback>
-            </Avatar>
-            <span
-              className={`-bottom-0.5 -right-0.5 absolute size-3 rounded-full border-2 border-background ${
-                status === "online"
-                  ? "bg-green-500"
-                  : status === "offline"
-                  ? "bg-gray-400"
-                  : "bg-violet-500"
-              }`}
-            />
-          </div>
-          <span
-            className={`text-base  ${
-              hovered ? "text-black" : "text-muted-foreground"
-            }`}
-          >
-            {first_name + " " + last_name}
-          </span>
-        </a>
-        <X
-          className={`transition-all duration-200 cursor-pointer  ${
-            hovered ? "inline-block opacity-100" : "hidden opacity-0"
-          }`}
-        />
-      </SidebarMenuButton>
-    </SidebarMenuItem>
-  );
+	return (
+		<SidebarMenuItem
+			key={id}
+			className="user-menu-item cursor-pointer group/item"
+		>
+			<SidebarMenuButton className="gap-4" size={"lg"}>
+				<Link
+					href={url}
+					className="transition-all duration-200 w-full h-full flex items-center gap-4"
+				>
+					<div className="relative w-fit">
+						<Avatar className="rounded-full border">
+							<AvatarImage alt="cn" src={image} />
+							<AvatarFallback className="rounded-full">
+								{initials}
+							</AvatarFallback>
+						</Avatar>
+						<span
+							className={`-bottom-0.5 -right-0.5 absolute size-3 rounded-full border-2 border-background ${
+								status === "online"
+									? "bg-green-500"
+									: status === "offline"
+									? "bg-gray-400"
+									: "bg-violet-500"
+							}`}
+						/>
+					</div>
+					{/* text-black */}
+					<span
+						className={
+							"text-base text-muted-foreground group-hover/item:text-black"
+						}
+					>
+						{first_name + " " + last_name}
+					</span>
+				</Link>
+				{/*  */}
+				<X
+					className={
+						"transition-all duration-200 cursor-pointer hidden opacity-0 group-hover/item:inline-block group-hover/item:opacity-100"
+					}
+				/>
+			</SidebarMenuButton>
+		</SidebarMenuItem>
+	);
 };
 
 export default UserSidebarItem;
